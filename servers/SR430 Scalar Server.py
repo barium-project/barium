a2df4a6e9ff4cfd230223446b16e1ff1c7a86a9b
calvinzhe@g.ucla.edu <calvinzhe@github.com>
2016-03-15 16:56:03 -0700

    bins_per_record and bin_width now have supported argument conditions statistics now sets llim/rlim and returns counts start_new_can now sleeps for the total count time error now reports standard events a few minor changes to function descriptions

--- servers/SR430 Scalar Server.py
+++ servers/SR430 Scalar Server.py
@@ -31,22 +31,24 @@ class SR430_Scalar_Server(GPIBManagedServer):
         '''
         dev = self.selectedDevice(c)
         yield dev.write('*IDN?')
-        sleep(0.1)
         message = yield dev.read()
         returnValue(message)
 
     @setting(11, 'Trigger Level', value='v[V]', returns='s')
     def trigger_level(self, c, value=None):
         '''
-        Sets the trigger level.
-
+        Sets or queries the trigger level.
+         1 argument sets.  No arguments queries.
+         Acceptable range: [-2.000,2.000]
          Equivalent to TRLV.
         '''
         dev = self.selectedDevice(c)
         if value==None: # Function behaves as query when given no arguments
             yield dev.write('TRLV?')
-            sleep(0.1)
             message = yield dev.read()
+            returnValue(message)
+        elif value > 2 or value < 2:
+            message = 'Input out of range.  Acceptable range: [-2.000,2.000]'
             returnValue(message)
         else:
             yield dev.write('TRLV '+str(value['V'])) #Converts value in volts to string
@@ -55,13 +57,13 @@ class SR430_Scalar_Server(GPIBManagedServer):
     def discriminator_level(self, c, value=None):
         '''
         Sets or queries the discriminator level.
+         1 argument sets.  No arguments queries.
          Acceptable range: [-0.300,0.300]
          Equivalent to DCLV.
         '''
         dev = self.selectedDevice(c)
         if value==None:
             yield dev.write('DCLV?')
-            sleep(0.1)
             message = yield dev.read()
             returnValue(message)
         else:
@@ -71,17 +73,17 @@ class SR430_Scalar_Server(GPIBManagedServer):
     def records_per_scan(self, c, value=None):
         '''
         Sets or queries the records per scan
+         1 argument sets.  No arguments queries.
          Acceptable range: [0,65535]
          Equivalent to RSCN.
         '''
         dev = self.selectedDevice(c)
         if value==None:
             yield dev.write('RSCN?')
-            sleep(0.1)
             message = yield dev.read()
             returnValue(message)
         elif value > 65535:
-            message = "Input out of range.  Acceptable range: [0,65535]
+            message = "Input out of range.  Acceptable range: [0,65535]"
             returnValue(message)
         else:
             yield dev.write('RSCN '+str(value))
@@ -89,7 +91,8 @@ class SR430_Scalar_Server(GPIBManagedServer):
     @setting(14, 'Output GPIB')
     def output_gpib(self, c):
         '''
-        Sets query output to GPIB
+        Sets query output to GPIB.  This is necessary so that any queries to the
+         scalar will output to the GPIB port.
          
          Equivlanet to OUTP 1.
         '''
@@ -127,76 +130,108 @@ class SR430_Scalar_Server(GPIBManagedServer):
         dev = self.selectedDevice(c)
         yield dev.write('CLRS')
 
-    @setting(18, 'Start New Scan')
+    @setting(18, 'Start New Scan', returns='s')
     def start_new_scan(self, c):
         '''
         Clears current data and starts new scan.
+         The time of scan is calculated using (bins per record) * (bin width).
          Uses both clear_scan() and start_scan() methods.
         '''
-        dev = self.selectedDevice(c)
-        yield self.clear_scan(c)
-        sleep(0.01)
-        yield self.start_scan(c)
+        if duration < 0:
+            message = 'Input out of range.  Range: [0, Infinity)'
+            returnValue(message)
+        else:
+            dev = self.selectedDevice(c)
+            
+            bins_per_record = yield self.bins_per_record(0)     #Looks up bins per record
+            bin_width = yield self.bin_width(0)                 #Looks up bin width (time duration)
+            counting_wait_time = bins_per_record*bin_width*1e9  #Total scan time = bpr*bwidth
+            
+            yield self.clear_scan(c)    #Clears the scan of previous data
+            yield self.start_scan(c)    #Starts new scan
+            sleep(counting_wait_time)
 
-    @setting(19, 'Statistics')
+    @setting(19, 'Statistics',returns='w')
     def statistics(self, c):
         '''
         Performs statistics.
+         First the left limit is set to 0 and the right limit is set to the last bin (based on bins per record).
+         Then statistics are done on this range, which is basically all of the bins, with a wait time of 1s.
+         Then the number of counts is returned as an integer.
 
-         Equivalent to STAT
+         SR430 commands used: LLIM, RLIM, STAT, and SPAR?2
         '''
         dev = self.selectedDevice(c)
+        yield dev.write('LLIM 0')                       #Sets the left limit as bin 0
+        bins_per_record = yield self.bins_per_record(0) #Looks up bins per record (total number of bins)
+        last_bin_index = bins_per_record - 1
+        yield dev.write('RLIM '+str(last_bin_index))    #Lets the right limit as the total number of bins - 1 (last bin)
         yield dev.write('STAT')
+        sleep(1)
+        yield dev.write('SPAR?2')           #Queries for the total # of counts (SPAR?2) See Manual
+        number_of_counts = yield dev.read() #Reads the buffer for total # of counts
+        returnValue(int(number_of_counts))  #Returns total # of counts as an integer
 
     @setting(20, 'Bins Per Record', value='w', returns='s')
     def bins_per_record(self, c, value=None):
         '''
         Sets or queries bins per record.
-         1 argumements sets.  0 arguments queries.
-
-         Equivlanet to BREC
+         1 argument (with value!=0) sets.
+         1 argument (with value=0) queries.
+         No arguments returns a list of supported arguments.
+         Accepts integer multiples of 1024 as arguments up to 16*1024.
+         Example: bins_per_record(1024) sets the bins per record to 1024 steps.
+        
+         Equivalent to BREC
         '''
         dev = self.selectedDevice(c)
+        supported_arguments = [1024,2*1024,3*1024,4*1024,5*1024,
+                               6*1024,7*1024,8*1024,9*1024,10*1024,
+                               11*1024,12*1024,13*1024,14*1024,15*1024,
+                               16*1024]
         if value==None:
+            message = 'Supported arguments: '+str(supported_arguments)
+            returnValue(message)
+        elif value==0:
             yield dev.write('BREC?')
-            sleep(0.1)
             message = yield dev.read()
             returnValue(message)
-        elif value==0 or value>16:
-            message = 'Input out of range.  Acceptable range: [0,16]'
-            returnValue(message)
-        else:
+        elif value in supported_arguments:
             yield dev.write('BREC '+str(value))
+        else:
+            message = 'Unsupported argument. Supported arguments: '+str(supported_arguments)
+            returnValue(message)
 
     @setting(21, 'Bin Width', value='w', returns='s')
     def bin_width(self, c, value=None):
         '''
-        Sets or queries bin width
-         1 argument sets.  0 arguments queries.
+        Sets or queries bin width (unsigned integervalue representing bin width in ns)
+         1 argument (with value!=0) sets.
+         1 argument (with value=0) queries.
+         No arguments returns a list of supported arguments.
+         Supported arguments: [5,40,80,160,320,640,1280,2560,5120,
+                               10240,20480,40960,81920,163840,327680,
+                               655360,1310700,2621400,5242900,1048600]
+         Example: bin_width(5) sets the bin width to 5 ns.
 
          Equivalent to BWTH
         '''
         dev = self.selectedDevice(c)
+        supported_arguments = [5,40,80,160,320,640,1280,2560,5120,
+                               10240,20480,40960,81920,163840,327680,
+                               655360,1310700,2621400,5242900,1048600]
         if value==None:
+            message = 'Supported arguments: '+str(supported_arguments)
+            returnValue(message)
+        elif value==0:
             yield dev.write('BWTH?')
-            sleep(0.1)
             message = yield dev.read()
             returnValue(message)
-        elif value > 19:
-            message = 'Input out of range.  Acceptable range: [0,19]'
-            returnValue(message)
-        else:
+        elif value in supported_arguments:
             yield dev.write('BWTH '+str(value))
-
-    @setting(22, 'Reset')
-    def reset(self, c):
-        '''
-        Reset to default configurations.
-
-         Equivalent to *RST
-        '''
-        dev = self.selectedDevice(c)
-        yield dev.write('*RST')
+        else:
+            message = 'Unsupported argument.  Supported arguments: '+str(supported_arguments)
+            returnValue(message)
 
     @setting(23, 'Clear Status')
     def clear_status(self, c):
@@ -211,23 +246,36 @@ class SR430_Scalar_Server(GPIBManagedServer):
     @setting(24, 'Error', returns='s')
     def error(self, c):
         '''
-        Reads and reports erros.
-        0 Plot Error;  1 Print Error;  2 Memory Error; 3 Disk Error;
-        4 Unused;  5 Clock Unlock;  6 Rate Error;  7 Overflow
+        Reads and reports standard event errors.
 
-         Equivalent to ERRS?
+         Equivalent to ESE?
         '''
         dev = self.selectedDevice(c)
-        yield dev.write('ERRS?')
-        sleep(0.1)
-        message = yield dev.read()
+        yield dev.write('ESE? 0')
+        input_error = yield dev.read()
+        yield dev.write('ESE? 2')
+        query_error = yield dev.read()
+        yield dev.write('ESE? 4')
+        execution_error = yield dev.read()
+        yield dev.write('ESE? 5')
+        command_error = yield dev.read()
+        yield dev.write('ESE? 6')
+        URQ = yield dev.read()
+        yield dev.write('ESE? 7')
+        PON = yield dev.read()
+        if input_error==0 and query_error==0 and execution_error==0 and command_error==0 and URQ ==0 and PON ==0:
+            message = 'No error.'
+        else:
+            message = ('Error bytes:  Input Error '+input_error+'; Query Error '+
+                        query_error+'; Execution Error '+execution_error+
+                        '; Command Error '+command_error+'; URQ '+URQ+'; PON '+PON)
         returnValue(message)
 
 
 
 if __name__ == "__main__":
-    from labard import util
-    util.runServer(SR430_Scalar_server())
+    from labrad import util
+    util.runServer(SR430_Scalar_Server())
             
 
 
