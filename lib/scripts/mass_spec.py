3b35e0b0b77476a9e9ed93650e7caf29e53ff3ca
barium-project <jchristensen@physics.ucla.edu>
2016-05-02 15:15:10 -0700

    Didn't actually add updated mass spec file.

--- lib/scripts/mass_spec.py
+++ lib/scripts/mass_spec.py
@@ -1,4 +1,22 @@
-### Script to take a mass spectrum
+### Script to take a mass spectrum on the SRS RGA200
+### This is written to work on a specific computer since the labrad
+### servers incorporate the computer name.
+
+# Parameter List, Only changes these values. Double click to run.
+##############################################################################
+rga_port = "COM1"
+labrad_manager_address = 'localhost'
+discriminator_level = .1 # In volts
+records_per_scan = 500
+mass_min = 17 #amu
+mass_max = 18 #amu
+number_of_points = 3
+# Time required for sr430 to count at each point. Must confirm this before you run.
+count_time = 60*3
+file_location = 'Z:/Group_Share/Barium/Data/2016/4/29'
+# Must enter the bias voltage on the filament
+bias_v = 7 # V
+##############################################################################
 
 import labrad
 import numpy as np
@@ -6,79 +24,61 @@ import time
 from datetime import datetime
 from labrad.units import WithUnit as U
 
-cxn = labrad.connect()
+
+cxn = labrad.connect(labrad_manager_address)
 print 'Connected to Labrad'
 
-
+# Connect to servers
 rga = cxn.planet_express_serial_server
-sca = cxn.planet_express_gpib_bus
+sr430 = cxn.sr430_scalar_server
 hp = cxn.hp6033a_server
 
 
 # Set the baudrate and address for the RGA
-rga.open()
+rga.open(rga_port)
 rga.baudrate(28800)
 
 
 # Set the GPIB address
 
-sca.address('GPIB0::1::INSTR')
+sr430.select_device()
 hp.select_device()
 
 
 # Set scalar parameters
 
-sca.write('dclv300e-3')# set discriminator level
-sca.write('rscn10000') # set records per scan
+sr430.discriminator_level(U(discriminator_level,'V'))
+sr430.records_per_scan(records_per_scan)
 
 
-# Select the mass range
-
-m_max = 140
-m_min = 131
-n_points = 100
-
-# Set how long to wait to count in sec
-
-count_time = 30*60
 
 #Initialize results array(mass,counts,day,hour,minute,second,voltage,current)
 results = np.array([[0,0,0,0,0,0,0,0]])
 
-masses = [134,138,145]
+masses = np.linspace(mass_min,mass_max,number_of_points)
 
-#mass = np.linspace(m_min,m_max,n_points)
+for i in range(len(masses)):
 
-for i in range(n_points):
+    rga.write_line('ml'+str(masses[i]))
+    time.sleep(1)
 
-    for j in range(len(masses)):
-        
-        rga.write_line('ml'+str(masses[j]))
-        time.sleep(1)
+    # Acquire the data
 
-        # Acquire the data
-
-        sca.write('clrs') # clear the last run
-        sca.write('sscn') # start scan
-        time.sleep(count_time) # wait for scan to finish
-        sca.write('stat') # Do the statistics
-        print "do stats"
-        time.sleep(3) # Give time to calculate
-        sca.write('spar?2') # get total counts
-        counts = float(sca.read(256))# read the counts
-        t = datetime.now().timetuple() # get the time
-        voltage = hp.get_voltage()['V'] # get power supply voltage and current
-        current = hp.get_current()['A']
-        new_data = np.array([[masses[j],counts,t[2],t[3],t[4],t[5],voltage,current]])
-        print new_data
-        results = np.concatenate((results,new_data),axis = 0)
-        np.savetxt('Z:/Group_Share/Barium/Data/2016/3/16/mass_spec_134_138_145_7Vbias_1640s_100ug.txt',results,fmt="%0.5e")
+    sr430.start_new_scan(U(count_time,'s'))
+    counts = sr430.get_counts()
+    t = datetime.now().timetuple() # get the time
+    voltage = hp.get_voltage()['V'] # get power supply voltage and current
+    current = hp.get_current()['A']
+    new_data = np.array([[masses[i],counts,t[2],t[3],t[4],t[5],voltage,current]])
+    print new_data
+    results = np.concatenate((results,new_data),axis = 0)
+    file_name = file_location+'/mass_spec_'+str(mass_min)+'amu_'+str(mass_max)+'amu_'+str(bias_v)+'Vbias_'+str(count_time)+'s.txt'
+    np.savetxt(file_name,results,fmt="%0.5e")
 
 
 # close ports
 rga.close()
 
 # Set the current to zero
-#c = U(0,'A')
-#hp.set_current(c)
-
+c = U(0,'A')
+hp.set_current(c)
