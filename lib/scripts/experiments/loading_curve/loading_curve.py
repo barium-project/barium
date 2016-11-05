4504d6ea1969b4d01d851259f893c447d5953ac4
theoriginaljuice <physics.justin.christensen@gmail.com>
2016-11-05 16:06:00 -0700

    Added to loading curve. Now can choose to chop while filament is on with PMT.

--- lib/scripts/experiments/loading_curve/loading_curve.py
+++ lib/scripts/experiments/loading_curve/loading_curve.py
@@ -8,6 +8,12 @@ import datetime as datetime
 from config.FrequencyControl_config import FrequencyControl_config
 
 class loading_curve(experiment):
+    '''
+    This experiment is designed to take a loading curve by specifying an
+    array of times to load for (filament on), and then look at the data.
+    The camera will take a picture before and after loading. The PMT will
+    will chop during or after loading based on user input.
+    '''
 
     name = 'Loading Curve'
 
@@ -22,6 +28,7 @@ class loading_curve(experiment):
     exp_parameters.append(('Loading_Curve', 'Even_Isotope_Heating_While_Loading'))
     exp_parameters.append(('Loading_Curve', 'Source'))
     exp_parameters.append(('Loading_Curve', 'Subtract_BG'))
+    exp_parameters.append(('Loading_Curve', 'Chop_While_Heating'))
 
 
     @classmethod
@@ -47,21 +54,38 @@ class loading_curve(experiment):
         self.get_device_map()
 
     def run(self, cxn, context):
-        self.set_up_datavault()
         self.set_up_parameters()
+        self.set_up_datavault()
 
         # Get a background image if using the camera
         if self.source == 'Camera':
             self.bg_image = self.get_image()
 
+        elif self.source == 'PMT':
+            # Make sure PMT is recording data
+            if not self.pmt.isrunning():
+                self.pmt.record_data()
+
+            # Start a new data set to watch on grapher
+            self.pmt.start_new_dataset()
+
         for i in range(len(self.time_arr)):
             if self.pause_or_stop():
                 break
+
+            # Set the pmt mode
+            if self.source == 'PMT':
+                if int(self.chop_while_heating) == 1:
+                    self.pmt.set_mode('Differential')
+                else:
+                    self.pmt.set_mode('Normal')
+
 
             # Turn on the sideband for even isotope heating
             if int(self.heating) == 1:
                 self.hpa.select_device(self.device_mapA['GPIB0::19'])
                 self.hpa.rf_state(True)
+
 
             # run the filament
             self.run_filament(self.time_arr[i])
@@ -84,11 +108,18 @@ class loading_curve(experiment):
                 self.trap.trigger_hv_pulse()
 
             elif self.source == 'PMT':
+                # Start Chopping if we weren't
+                if int(self.chop_while_heating) == 0:
+                    self.pmt.set_mode('Differential')
+
                 for j in range(20):
                     counts_on = self.pmt.get_next_counts('ON', 1, False)
                     counts_off = self.pmt.get_next_counts('OFF', 1, False)
                     counts_diff = self.pmt.get_next_counts('DIFF', 1, False)
                     self.dv.add(self.time_arr[i],counts_on, counts_off, counts_diff)
+
+        if self.source == 'PMT':
+            self.pmt.set_mode('Normal')
 
     def set_up_datavault(self):
         # set up folder
@@ -98,9 +129,11 @@ class loading_curve(experiment):
         day   = '%02d' % date.day    # Padded with a zero if one digit
         trunk = year + '_' + month + '_' + day
         self.dv.cd(['',year,month,trunk],True)
-        dataset = self.dv.new('Loading_Curve',[('Time', 's')], [('KiloCounts/sec','Differential High','num'), \
+        if self.source == 'PMT':
+            dataset = self.dv.new('Loading_Curve',[('Time', 's')], [('KiloCounts/sec','Differential High','num'), \
                             ('KiloCounts/sec','Differential Low','num'),('KiloCounts/sec','Differential','num')])
-
+        else:
+            dataset = self.dv.new('Loading_Curve',[('Time', 's')], [('', 'Counts', 'a.b.u')])
         # add dv params
         self.dv.add_parameter('Loading_Current', self.parameters.Loading_Curve.Loading_Current)
         self.dv.add_parameter('Loading_Voltage',self.parameters.Loading_Curve.Loading_Voltages)
@@ -112,6 +145,7 @@ class loading_curve(experiment):
         self.dv.add_parameter('A_Ramp_Voltage', self.parameters.Loading_Curve.A_Ramp_Voltage)
         self.dv.add_parameter('A_Ramp_Time', self.parameters.Loading_Curve.A_Ramp_Time)
         self.dv.add_parameter('Even_Isotope_Heating', self.parameters.Loading_Curve.Even_Isotope_Heating_While_Loading)
+        self.dv.add_parameter('Chop_While_Heating', self.parameters.Loading_Curve.Chop_While_Heating)
 
     def set_up_parameters(self):
         self.current = self.parameters.Loading_Curve.Loading_Current
@@ -122,6 +156,7 @@ class loading_curve(experiment):
         self.a_ramp_time = self.parameters.Loading_Curve.A_Ramp_Time
         self.heating = self.parameters.Loading_Curve.Even_Isotope_Heating_While_Loading
         self.source = self.parameters.Loading_Curve.Source
+        self.chop_while_heating = self.parameters.Loading_Curve.Chop_While_Heating
 
         self.min_time = self.parameters.Loading_Curve.Loading_Time[0]['s']
         self.max_time = self.parameters.Loading_Curve.Loading_Time[1]['s']
