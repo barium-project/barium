e23ae7dd4239d1ff963dc98ed2df7c87aa706558
theoriginaljuice <physics.justin.christensen@gmail.com>
2017-01-20 14:02:52 -0800

    Fixed default setup faliure

--- lib/clients/FrequencyControl_client/FrequencyControl_client.py
+++ lib/clients/FrequencyControl_client/FrequencyControl_client.py
@@ -16,16 +16,17 @@ import numpy as np
 class FrequencyControlClient(Frequency_Ui):
 
     def __init__(self, reactor, parent=None):
-        """initializels the GUI creates the reactor
+        """initialize the GUI creates the reactor
 
         """
         super(FrequencyControlClient, self).__init__()
         self.password = os.environ['LABRADPASSWORD']
         #self.setSizePolicy(QtGui.QSizePolicy..Minimum, QtGui.QSizePolicy.Fixed)
         self.reactor = reactor
-        self.name = socket.gethostname() + ' Trap Control Client'
+        self.name = socket.gethostname() + ' Frequency Control Client'
         self.device_mapA = {}
         self.device_mapB = {}
+        self.context_b = {}
         self.setupUi()
         self.connect()
         #load default parameters and initialize the devices off
@@ -50,37 +51,52 @@ class FrequencyControlClient(Frequency_Ui):
         """
         self.serverIP = FrequencyControl_config.ip
         from labrad.wrappers import connectAsync
-        self.cxn = yield connectAsync(self.serverIP,
+        self.cxn6 = yield connectAsync(self.serverIP,
                                       name=self.name,
                                       password=self.password)
+        self.hp8675b_6 = self.cxn6.hp8657b_server
 
-        self.hp8672a_server = yield self.cxn.hp8672a_server
-        self.hp8657b_server = yield self.cxn.hp8657b_server
+        self.cxn7 = yield connectAsync(self.serverIP,
+                                      name=self.name,
+                                      password=self.password)
+        self.hp8675b_7 = self.cxn7.hp8657b_server
+
+        self.cxn8 = yield connectAsync(self.serverIP,
+                                      name=self.name,
+                                      password=self.password)
+        self.hp8675b_8 = self.cxn8.hp8657b_server
+
+
+
+        self.clients = [self.hp8675b_6, self.hp8675b_7, self.hp8675b_8]
 
         self.connectHPGUI()
+
 
     @inlineCallbacks
     def connectHPGUI(self):
 
-        gpib_listA = FrequencyControl_config.gpibA
+        #gpib_listA = FrequencyControl_config.gpibA
         gpib_listB = FrequencyControl_config.gpibB
-
+        '''
         devices = yield self.hp8672a_server.list_devices()
         for i in range(len(gpib_listA)):
             for j in range(len(devices)):
                 if devices[j][1].find(gpib_listA[i]) > 0:
                     self.device_mapA[gpib_listA[i]] = devices[j][0]
                     break
-
-        devices = yield self.hp8657b_server.list_devices()
+        '''
+        devices = yield self.clients[0].list_devices()
         for i in range(len(gpib_listB)):
             for j in range(len(devices)):
                 if devices[j][1].find(gpib_listB[i]) > 0:
                     self.device_mapB[gpib_listB[i]] = devices[j][0]
+                    self.clients[i].select_device(devices[j][1])
                     break
 
-        print self.device_mapA
+        #print self.device_mapA
         print self.device_mapB
+        '''
         # set up hp8672a oscillators
         self.GPIB19spinFreq.valueChanged.connect(lambda freq = \
                 self.GPIB19spinFreq.value(), device = self.device_mapA['GPIB0::19'] : self.freqChangedHPA(freq, device))
@@ -105,38 +121,39 @@ class FrequencyControlClient(Frequency_Ui):
         self.GPIB21switch.clicked.connect(lambda state = self.GPIB21switch.isChecked(), \
                 device = self.device_mapA['GPIB0::21'] : self.setRFHPA(device, state))
 
-
+        '''
         # set up hp8672b oscillators
         self.GPIB6spinFreq.valueChanged.connect(lambda freq = \
-                self.GPIB6spinFreq.value(), device = self.device_mapB['GPIB0::6'] : self.freqChangedHPB(freq, device))
+                self.GPIB6spinFreq.value(), client = self.clients[0] : self.freqChangedHPB(freq, client))
 
         self.GPIB6spinAmp.valueChanged.connect(lambda amp = self.GPIB6spinAmp.value(), \
-                device = self.device_mapB['GPIB0::6'] : self.ampChangedHPB(amp, device))
+                client = self.clients[0] : self.ampChangedHPB(amp, client))
 
         self.GPIB6switch.clicked.connect(lambda state = self.GPIB6switch.isChecked(), \
-                device = self.device_mapB['GPIB0::6'] : self.setRFHPB(device, state))
+                client = self.clients[0] : self.setRFHPB(client, state))
 
 
         self.GPIB7spinFreq.valueChanged.connect(lambda freq = \
-                self.GPIB7spinFreq.value(), device = self.device_mapB['GPIB0::7'] : self.freqChangedHPB(freq, device))
+                self.GPIB7spinFreq.value(), client = self.clients[1] : self.freqChangedHPB(freq, client))
 
         self.GPIB7spinAmp.valueChanged.connect(lambda amp = self.GPIB7spinAmp.value(), \
-                device = self.device_mapB['GPIB0::7'] : self.ampChangedHPB(amp, device))
+                client = self.clients[1] : self.ampChangedHPB(amp, client))
 
         self.GPIB7switch.clicked.connect(lambda state = self.GPIB7switch.isChecked(), \
-                device = self.device_mapB['GPIB0::7'] : self.setRFHPB(device, state))
+                client = self.clients[1] : self.setRFHPB(client, state))
 
 
         self.GPIB8spinFreq.valueChanged.connect(lambda freq = \
-                self.GPIB8spinFreq.value(), device = self.device_mapB['GPIB0::8'] : self.freqChangedHPB(freq, device))
+                self.GPIB8spinFreq.value(), client = self.clients[2] : self.freqChangedHPB(freq, client))
 
         self.GPIB8spinAmp.valueChanged.connect(lambda amp = self.GPIB8spinAmp.value(), \
-                device = self.device_mapB['GPIB0::8'] : self.ampChangedHPB(amp, device))
+                client = self.clients[2] : self.ampChangedHPB(amp, client))
 
         self.GPIB8switch.clicked.connect(lambda state = self.GPIB8switch.isChecked(), \
-                device = self.device_mapB['GPIB0::8'] : self.setRFHPB(device, state))
+                client = self.clients[2] : self.setRFHPB(client, state))
 
 
+        self.setDefault()
 
     def set19Default(self, params, state):
         self.GPIB19spinFreq.setValue(params['GPIB0::19'][0])
@@ -146,42 +163,30 @@ class FrequencyControlClient(Frequency_Ui):
         #self.ampChangedHPA19(self.device_mapA['GPIB0::19'])
         #self.setRFHPA(self.device_mapA['GPIB0::19'],state)
 
-    def set21Default(self, params, state):
-
+    def setDefault(self):
+        '''
         self.GPIB21spinFreq.setValue(params['GPIB0::21'][0])
         #self.freqChangedHPA(params['GPIB0::21'][0],self.device_mapA['GPIB0::21'] )
         self.GPIB21spinAmpDec.setValue(params['GPIB0::21'][1])
         self.GPIB21spinAmpVer.setValue(params['GPIB0::21'][2])
         #self.ampChangedHPA21(self.device_mapA['GPIB0::21'])
         #self.setRFHPA(self.device_mapA['GPIB0::21'],state)
-
-
-
         '''
-        print self.device_mapB
-        time.sleep(1)
-        self.GPIB6spinFreq.setProperty("value", params['GPIB0::6'][0])
-        #self.freqChangedHPB(params['GPIB0::6'][0],self.device_mapB['GPIB0::6'] )
-        self.GPIB6spinAmp.setProperty("Value", params['GPIB0::6'][1])
-        #self.ampChangedHPB(params['GPIB0::6'][1],self.device_mapB['GPIB0::6'] )
-        # self.setRFHPB(self.device_mapB['GPIB0::6'],state)
 
-        print self.device_mapB
-        time.sleep(1)
-        self.GPIB7spinFreq.setValue(params['GPIB0::7'][0])
-        #self.freqChangedHPB(params['GPIB0::7'][0],self.device_mapB['GPIB0::7'] )
-        self.GPIB7spinAmp.setValue(params['GPIB0::7'][1])
-        #self.ampChangedHPB(params['GPIB0::7'][1],self.device_mapB['GPIB0::7'] )
-        self.setRFHPB(self.device_mapB['GPIB0::7'],state)
-        print self.device_mapB
 
-        time.sleep(1)
-        #self.GPIB8spinFreq.setValue(params['GPIB0::8'][0])
-        self.freqChangedHPB(params['GPIB0::8'][0],self.device_mapB['GPIB0::8'] )
-        #self.GPIB8spinAmp.setValue(params['GPIB0::8'][1])
-        self.ampChangedHPB(params['GPIB0::8'][1],self.device_mapB['GPIB0::8'] )
-        self.setRFHPB(self.device_mapB['GPIB0::8'],state)
-       '''
+        self.GPIB6spinFreq.setValue(self.default['GPIB0::6'][0])
+        self.GPIB6spinAmp.setValue(self.default['GPIB0::6'][1])
+        self.setRFHPB(self.clients[0],False)
+
+        self.GPIB7spinFreq.setValue(self.default['GPIB0::7'][0])
+        self.GPIB7spinAmp.setValue(self.default['GPIB0::7'][1])
+        self.setRFHPB(self.clients[1],False)
+
+
+
+        self.GPIB8spinFreq.setValue(self.default['GPIB0::8'][0])
+        self.GPIB8spinAmp.setValue(self.default['GPIB0::8'][1])
+        self.setRFHPB(self.clients[2],False)
 
     @inlineCallbacks
     def freqChangedHPA(self, freq, device):
@@ -218,23 +223,20 @@ class FrequencyControlClient(Frequency_Ui):
 
     #hp8657b
     @inlineCallbacks
-    def freqChangedHPB(self, freq, device):
+    def freqChangedHPB(self, freq, client):
         from labrad.units import WithUnit as U
         frequency = U(freq,'MHz')
-        yield self.hp8657b_server.select_device(device)
-        yield self.hp8657b_server.set_frequency(frequency)
+        yield client.set_frequency(frequency)
 
     @inlineCallbacks
-    def ampChangedHPB(self, amp, device):
+    def ampChangedHPB(self, amp, client):
         from labrad.units import WithUnit as U
-        yield self.hp8657b_server.select_device(device)
         amp = U(amp,'dBm')
-        yield self.hp8657b_server.set_amplitude(amp)
+        yield client.set_amplitude(amp)
 
     @inlineCallbacks
-    def setRFHPB(self, device, state):
-        yield self.hp8657b_server.select_device(device)
-        yield self.hp8657b_server.rf_state(state)
+    def setRFHPB(self, client, state):
+        yield client.rf_state(state)
 
 
 
