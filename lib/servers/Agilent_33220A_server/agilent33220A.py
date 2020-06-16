607c18cd288b412978d7889e21159adaac0f4990
theoriginaljuice <physics.justin.christensen@gmail.com>
2020-06-16 15:20:25 -0700

    Adding agilent server v1

---
+++ lib/servers/Agilent_33220A_server/agilent33220A.py
@@ -0,0 +1,271 @@
+# Copyright (C) 2011 Anthony Ransford
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+"""
+### BEGIN NODE INFO
+[info]
+name = Agilent 33220A Server
+version = 1.3
+description = 
+
+[startup]
+cmdline = %PYTHON% %FILE%
+timeout = 20
+
+[shutdown]
+message = 987654321
+timeout = 5
+### END NODE INFO
+"""
+
+from labrad.server import setting
+from labrad.gpib import GPIBManagedServer, GPIBDeviceWrapper
+from twisted.internet.defer import inlineCallbacks, returnValue
+
+class AgilentWrapper(GPIBDeviceWrapper):
+    
+    def initialize(self):
+        '''
+        Provides a lookup table for waveform to GPIB lingo
+        '''
+        self.lookup = {'sine':'SIN', 'square':'SQU', 'ramp':'RAMP', 'pulse':'PULS', 'noise':'NOIS', 'DC' : 'DC', 'USER':'USER'}
+        
+
+    
+    @inlineCallbacks
+    def Output(self, output = None):
+        '''
+        Turns on or off the rigol output of specified channel
+        '''
+
+        if output == True:
+            yield self.write("OUTP" + " ON")
+        elif output == False:
+            yield self.write("OUTP" + " OFF")
+        else:
+            yield self.write("OUTP" + "?")
+            state = yield self.read()
+            returnValue(state)
+            
+            
+    @inlineCallbacks
+    def applyWaveForm(self, function, frequency, amplitude, offset):
+        '''
+        Applys waveform from self.lookup dictionary with given parameters
+        '''
+        output = "APPL:" + self.lookup[function]  + ' ' + str(int(frequency['Hz'])) + ',' + str(amplitude['V']) + ',' + str(offset['V'])
+        yield self.write(output)
+     
+    @inlineCallbacks    
+    def WaveFunction(self, function = None):
+        '''
+        Changes wave form
+        '''
+        if function == None:
+            output = "FUNC" + "?"
+            yield self.write(output)
+            func = yield self.read()
+            returnValue(func)
+        else:
+            output = "FUNC"  + " " + self.lookup[function]
+            yield self.write(output)
+
+        
+    @inlineCallbacks
+    def Frequency(self, frequency = None):
+        '''
+        Sets frequency
+        '''
+        if frequency == None:
+            output = "FREQ" +"?"
+            yield self.write(output)
+            freq = yield  self.read()
+            returnValue(freq)
+        else:
+            output = "FREQ " + str(int(frequency['Hz']))
+            yield self.write(output)
+
+    @inlineCallbacks
+    def setDC(self, voltage = None):
+        '''
+        sets DC output value
+        '''
+        if voltage == None:
+            output = "VOLT:OFFS" 
+            yield self.write(output)
+            volts = self.read()
+            returnValue(volts)
+        else:
+            output = 'APPL:DC' + ' DEF,DEF,' + str(voltage['V'])
+            yield self.write(output)
+        
+        
+    @inlineCallbacks
+    def Amplitude(self, voltage = None):
+        '''
+        sets amp
+        '''
+        if voltage == None:
+            output = "VOLT"  + "?"
+            yield self.write(output)
+            volts = yield self.read()
+            returnValue(volts)
+        else:
+            output = "VOLT"  + " " + str(voltage['V'])
+            yield self.write(output)
+
+    @inlineCallbacks
+    def AMSource(self, source):
+        '''
+        Select internal or external modulation source, the default is INT
+        '''
+        output = "AM:SOUR " + source
+        yield self.write(output)
+        
+    @inlineCallbacks
+    def AMFunction(self, function):
+        '''
+        Select the internal modulating wave of AM
+        In internal modulation mode, the modulating wave could be sine,
+        square, ramp, negative ramp, triangle, noise or arbitrary wave, the
+        default is sine.
+        '''
+        output = "AM:INT:FUNC " + self.lookup[function]
+        yield self.write(output)
+        
+    @inlineCallbacks
+    def AMFrequency(self, frequency):
+        '''
+        Set the frequency of AM internal modulation in Hz
+        Frequency range: 2mHz to 20kHz
+        '''
+        output = "AM:INT:FREQ " + str(frequency['Hz'])
+        yield self.write(output)
+        
+    @inlineCallbacks
+    def AMDepth(self, depth):
+        '''
+        Set the depth of AM internal modulation in percent
+        Depth range: 0% to 120%
+        '''
+        output = "AM:DEPT " + str(depth)
+        yield self.write(output)
+        
+    @inlineCallbacks
+    def AMState(self, state):
+        '''
+        Disable or enable AM function
+        '''
+        if state:
+            state = 'ON'
+        else:
+            state = 'OFF'
+            
+        output = "AM:STAT " + state
+        yield self.write(output)
+
+    @inlineCallbacks
+    def FMSource(self, source):
+        '''
+        Select internal or external modulation source, the default is INT
+        '''
+        output = "FM:SOUR " + source
+        yield self.write(output)
+        
+    @inlineCallbacks
+    def FMFunction(self, function):
+        '''
+        In internal modulation mode, the modulating wave could be sine,
+        square, ramp, negative ramp, triangle, noise or arbitrary wave, the
+        default is sine
+        '''
+        output = "FM:INT:FUNC " + self.lookup[function]
+        yield self.write(output)
+        
+    @inlineCallbacks
+    def FMFrequency(self, frequency):
+        '''
+        Set the frequency of FM internal modulation in Hz
+        Frequency range: 2mHz to 20kHz
+        '''
+        output = "FM:INT:FREQ " + str(frequency['Hz'])
+        yield self.write(output)
+        
+    @inlineCallbacks
+    def FMDeviation(self, deviation):
+        '''
+        Set the frequency deviation of FM in Hz.
+        '''
+        output = "FM:DEV " + str(deviation)
+        yield self.write(output)
+        
+    @inlineCallbacks
+    def FMState(self, state):
+        '''
+        Disable or enable FM function
+        '''
+        if state:
+            state = 'ON'
+        else:
+            state = 'OFF'
+            
+        output = "FM:STAT " + state
+        yield self.write(output)
+        
+
+class AgilentServer(GPIBManagedServer):
+    name = 'Agilent 33220A Server' # Server name
+    deviceName = 'AGILENT TECHNOLOGIES 33220A' # Model string returned from *IDN?
+    deviceWrapper = AgilentWrapper
+
+    @setting(10, 'Output', channel = 'i', output = 'b')
+    def deviceOutput(self, c, channel, output = None): # uses passed context "c" to address specific device 
+        dev = self.selectedDevice(c)
+        yield dev.Output(output)
+    
+    @setting(69, 'Apply Waveform', function = 's', frequency = ['v[Hz]'], amplitude = ['v[V]'], offset = ['v[V]']  )
+    def applyDeviceWaveform(self, c, function, frequency, amplitude, offset):
+        dev = self.selectedDevice(c)
+        yield dev.applyWaveForm(function, frequency, amplitude, offset)
+        
+    @setting(707, 'Wave Function', function = 's')
+    def deviceFunction(self, c, function = None):
+        dev = self.selectedDevice(c)
+        func = yield dev.WaveFunction(function)
+        returnValue(func)
+
+    @setting(131, 'Amplitude', value = 'v[V]')
+    def Amplitude(self, c, value = None):
+        dev = self.selectedDevice(c)
+        volts = yield dev.Amplitude(value)
+        returnValue(volts)
+
+    @setting(92, 'Frequency', value = 'v[Hz]')
+    def Frequency(self, c, value = None):
+        dev = self.selectedDevice(c)
+        freq = yield dev.Frequency(value)
+        returnValue(freq)
+
+    @setting(9, 'Apply DC', value = 'v[V]')
+    def setDC(self, c, value = None):
+        dev = self.selectedDevice(c)
+        volts = yield dev.setDC(value)
+        returnValue(volts)
+
+__server__ = AgilentServer()
+
+if __name__ == '__main__':
+    from labrad import util
+    util.runServer(__server__)
