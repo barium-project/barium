40b17fcba87ef7b2b7096ebff8d7d02a92061089
Xueping Long <gregllong@gmail.com>
2020-06-26 08:18:55 -0700

    added node info

--- lib/servers/fiber_switch_server/fiber_switch_server.py
+++ lib/servers/fiber_switch_server/fiber_switch_server.py
@@ -1,8 +1,33 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan 27 16:36:29 2020
+# Copyright (C) 2020 Keqin Yan
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-@author: barium133
+"""
+### BEGIN NODE INFO
+[info]
+name = Fiber Switch Server
+version = 1.0
+description =
+
+[startup]
+cmdline = %PYTHON% %FILE%
+timeout = 20
+
+[shutdown]
+message = 987654321
+timeout = 5
+### END NODE INFO
 """
 
 from common.lib.servers.serialdeviceserver import SerialDeviceServer, setting, inlineCallbacks, SerialDeviceError, SerialConnectionError, PortRegError
@@ -48,8 +73,8 @@ class Fiber_Switch_Server( SerialDeviceServer ):
                 print 'Error opening serial connection'
                 print 'Check set up and restart serial server'
             else: raise
-        
-            
+
+
     def initContext(self, c):
         self.listeners.add(c.ID)
     def expireContext(self, c):        #Removes expired contexts from the listeners list?
@@ -59,9 +84,9 @@ class Fiber_Switch_Server( SerialDeviceServer ):
         if c.ID in notified:
             notified.remove(c.ID)
         return notified
-               
-        
-    @setting(1, channel1 = 'w')    
+
+
+    @setting(1, channel1 = 'w')
     def set_channel(self, c, channel1):
         """
         Switch between input channels on the optical fiber switch. Acceptable Range: [01, 08]
@@ -80,7 +105,7 @@ class Fiber_Switch_Server( SerialDeviceServer ):
         message = yield self.ser.read_line()
         try:
 #            if message[10] != num:
-#                raise except 
+#                raise except
             returnValue(int(message[10]))
         except:
             yield self.ser.write_line('<OSW_OUT_?>')
@@ -88,11 +113,11 @@ class Fiber_Switch_Server( SerialDeviceServer ):
             returnValue(int(message[10]))
         else:
             returnValue("Something went wrong, cannot get current channel")
-       
-        
+
+
 __server__ = Fiber_Switch_Server()
 
 if __name__ == "__main__":
     from labrad import util
     util.runServer(__server__)
- 
+
