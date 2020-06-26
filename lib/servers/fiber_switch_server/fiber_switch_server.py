15cc498d9278c2edab4681af0b6f02ca63b7f054
theoriginaljuice <physics.justin.christensen@gmail.com>
2020-06-26 10:14:47 -0700

    cleaned up unused code and tested

--- lib/servers/fiber_switch_server/fiber_switch_server.py
+++ lib/servers/fiber_switch_server/fiber_switch_server.py
@@ -30,16 +30,11 @@ timeout = 5
 ### END NODE INFO
 """
 
-from common.lib.servers.serialdeviceserver import SerialDeviceServer, setting, inlineCallbacks, SerialDeviceError, SerialConnectionError, PortRegError
-from labrad.types import Error
-from labrad.server import Signal
+from common.lib.servers.serialdeviceserver import SerialDeviceServer, setting,\
+inlineCallbacks, SerialDeviceError, SerialConnectionError
 from labrad import types as T
-from twisted.internet.task import LoopingCall
 from twisted.internet.defer import returnValue
 from labrad.support import getNodeName
-#import os
-
-#import time
 
 SERVERNAME = 'Fiber Switch Server'
 TIMEOUT = 1.0
@@ -52,13 +47,11 @@ class Fiber_Switch_Server( SerialDeviceServer ):
     port = None
     serNode = getNodeName()
     timeout = T.Value(TIMEOUT,'s')
-    #password = os.environ['LABRADPASSWORD']
-
-    listeners = set()
 
     @inlineCallbacks
     def initServer( self ):
-        if not self.regKey or not self.serNode: raise SerialDeviceError( 'Must define regKey and serNode attributes' )
+        if not self.regKey or not self.serNode: raise \
+        SerialDeviceError( 'Must define regKey and serNode attributes' )
         port = yield self.getPortFromReg( self.regKey )
         self.port = port
         try:
@@ -67,34 +60,26 @@ class Fiber_Switch_Server( SerialDeviceServer ):
         except SerialConnectionError, e:
             self.ser = None
             if e.code == 0:
-                print 'Could not find serial server for node: %s' % self.serNode
+                print 'Could not find serial server for node: %s' % \
+                self.serNode
                 print 'Please start correct serial server'
             elif e.code == 1:
                 print 'Error opening serial connection'
                 print 'Check set up and restart serial server'
             else: raise
 
-
-    def initContext(self, c):
-        self.listeners.add(c.ID)
-    def expireContext(self, c):        #Removes expired contexts from the listeners list?
-        self.listeners.remove(c.ID)
-    def getOtherListeners(self, c):    #Returns a list of listeners without the context itself.
-        notified = self.listeners.copy()
-        if c.ID in notified:
-            notified.remove(c.ID)
-        return notified
-
-
     @setting(1, channel1 = 'w')
     def set_channel(self, c, channel1):
         """
-        Switch between input channels on the optical fiber switch. Acceptable Range: [01, 08]
+        Switch between input channels on the optical fiber switch. 
+        Acceptable Range: [01, 08]
         """
-        yield self.ser.write_line('<OSW_OUT_0' + str(channel1) + '>')
+        
         if channel1 > 8 or channel1 < 1:
-            print 'Channel number needs to be from 1 to 8'
+            returnValue('Channel number needs to be from 1 to 8')
 
+        else:
+            yield self.ser.write_line('<OSW_OUT_0' + str(channel1) + '>')
 
     @setting(100, returns = 'w')
     def get_channel(self, c):
