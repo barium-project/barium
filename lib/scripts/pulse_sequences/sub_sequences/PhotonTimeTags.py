099c5a781d038c50d0c8f8ad7162a428537dd271
jchristensen133 <physics.justin.christensen@gmail.com>
2020-08-25 15:05:57 -0700

    photon correlation experiment for mm compensation

--- lib/scripts/pulse_sequences/sub_sequences/PhotonTimeTags.py
+++ lib/scripts/pulse_sequences/sub_sequences/PhotonTimeTags.py
@@ -1,4 +1,5 @@
 from common.lib.servers.Pulser2.pulse_sequences.pulse_sequence import pulse_sequence
+from labrad.units import WithUnit as U
 
 """
 We want to record photon counts only when we're probing the ion with the probe
@@ -13,7 +14,8 @@ class photon_timetags(pulse_sequence):
     required_parameters = [('PhotonTimeTags','record_timetags_duration')]
 
     def sequence(self):
-        # start time is defined to be 0s.
+        # start time is defined to be 1.0 us, but we want to start at 0.0s.
+        self.start = U(0.0, 's')
         p = self.parameters.PhotonTimeTags
         self.addTTL('TimeResolvedCount', self.start, p.record_timetags_duration)
         self.end = self.start + p.record_timetags_duration
